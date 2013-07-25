c02b2c0ed0116f9d82129f201016c8c37c667a65
developerscode <developerscode@yahoo.com>
2013-07-25 22:33:21 +0530

    1

---
+++ Javascript/MainScript.js
@@ -0,0 +1,20 @@
+﻿//Global Variables Declaration
+
+
+$(document).ready(function () {
+
+    $("#btnSearch").click(function () {
+    var keyword = $('#searchtext').val();
+    var region = $('#region_select').val();
+    var group = $('#catgroup').val();
+
+        window.open("ListingPage.html?q=" + keyword + "&ca=" + region + "&e=1&sa=&cg=" + group + "&female_subcat=&ps=&pe=&rs=&re=&o=0");
+
+    });
+    $("#btnback").click(function () {       
+
+        window.open("Search.html");
+
+    });
+
+});
